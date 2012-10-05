fc19b5832187f38819d0e00af1039586529e5824
Chris Wayne <chris.wayne@canonical.com>
2012-10-05 19:02:59 -0400

    First working commit

--- unity_lens_github/__init__.py
+++ unity_lens_github/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import optparse
+import urllib2
+import simplejson
 
 import gettext
 from gettext import gettext as _
@@ -15,19 +17,33 @@ class GithubLens(SingleScopeLens):
         name = 'github'
         description = 'Github Lens'
         search_hint = 'Search Github'
-        icon = 'github.svg'
-        search_on_blank=True
-
-    # TODO: Add your categories
-    example_category = ListViewCategory("Examples", 'help')
+        icon = 'github.png'
+        search_on_blank = False
+        
+    github_icon = '/usr/share/unity/lenses/github/github-logo.png'
+    repo_category = ListViewCategory("Repos", github_icon)
+    
+    
+    def search_repos(self, search):
+        try:
+            repo_url = 'https://api.github.com/legacy/repos/search/%s' % search
+            repos = simplejson.loads(urllib2.urlopen(repo_url).read())['repositories']
+            return repos
+        except (IOError, KeyError, urllib2.URLError, urllib2.HTTPError, simplejson.JSONDecodeError):
+            print "Error, unable to search github"
+            return []
 
     def search(self, search, results):
         # TODO: Add your search results
-        results.append('https://wiki.ubuntu.com/Unity/Lenses/Singlet',
-                         'ubuntu-logo',
-                         self.example_category,
-                         "text/html",
-                         'Learn More',
-                         'Find out how to write your Unity Lens',
-                         'https://wiki.ubuntu.com/Unity/Lenses/Singlet')
+        for repo in self.search_repos(search):
+            desc = ''
+            if repo['description']:
+                desc = repo['description']
+            results.append('http://github.com/%s/%s' % (repo['owner'], repo['name']),
+                            self.github_icon,
+                            self.repo_category,
+                            "text/html",
+                            '%s/%s' % (repo['owner'], repo['name']),
+                            desc,
+                            'http://github.com/%s/%s' % (repo['owner'], repo['name']))
         pass
